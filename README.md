b1eb85395858a61fd70e44e1d77c59a88bce2e2e
Abdul Muhaimin Toha <abdulmuhaimintoha.business@gmail.com>
2024-02-07 22:30:38 +0600

    final commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# PH-M4-Assignment-4
+
+## Basic Javascript
+
+Milestone: 4, Assignment: 4 (Basic Javascript). Javascript based assgnment for Progmming Hero course.
+
+[Project Repository Link](https://github.com/abdul-muhaimin-toha/PH-M4-Assignment-4)
---
+++ index.js
@@ -0,0 +1,69 @@
+function calculateMoney(sales) {
+  if (sales < 0) {
+    return 'Invalid Number';
+  }
+  return 120 * sales - (500 + 50 * 8);
+}
+
+function checkName(childName) {
+  if (typeof childName !== 'string') {
+    return 'Invalid';
+  }
+  if (
+    childName[childName.length - 1].toLowerCase() === 'a' ||
+    childName[childName.length - 1].toLowerCase() === 'y' ||
+    childName[childName.length - 1].toLowerCase() === 'i' ||
+    childName[childName.length - 1].toLowerCase() === 'e' ||
+    childName[childName.length - 1].toLowerCase() === 'o' ||
+    childName[childName.length - 1].toLowerCase() === 'u' ||
+    childName[childName.length - 1].toLowerCase() === 'w'
+  ) {
+    return 'Good Name';
+  }
+  return 'Bad Name';
+}
+
+function deleteInvalids(arr) {
+  const newArr = [];
+  if (!Array.isArray(arr)) {
+    return 'Invalid Array';
+  }
+  for (const item of arr) {
+    if (typeof item === 'number' && !isNaN(item)) {
+      newArr.push(item);
+    }
+  }
+  return newArr;
+}
+
+function password(obj) {
+  if (
+    obj.name === undefined ||
+    obj.birthYear === undefined ||
+    obj.siteName === undefined ||
+    !(obj.birthYear >= 1000)
+  ) {
+    return 'Invalid';
+  }
+  return `${obj.siteName[0].toUpperCase() + obj.siteName.slice(1)}#${
+    obj.name
+  }@${obj.birthYear}`;
+}
+
+function monthlySavings(arr, livingCost) {
+  let incomeAfterTax = 0;
+  if (!Array.isArray(arr) || typeof livingCost !== 'number') {
+    return 'Invalid input';
+  }
+  for (const item of arr) {
+    if (item >= 3000) {
+      incomeAfterTax += item - item * 0.2;
+    } else {
+      incomeAfterTax += item;
+    }
+  }
+  if (incomeAfterTax < livingCost) {
+    return 'Earn more';
+  }
+  return incomeAfterTax - livingCost;
+}
